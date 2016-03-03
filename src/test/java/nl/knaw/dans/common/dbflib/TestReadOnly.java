8e789241989ec3d2ea0c773b1cb58b748ea9be7d
Simon Chenery <simon_chenery@yahoo.com>
2016-03-03 07:28:08 +0100

    Use same DANS license header for mycila check

--- src/test/java/nl/knaw/dans/common/dbflib/TestReadOnly.java
+++ src/test/java/nl/knaw/dans/common/dbflib/TestReadOnly.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2016 Simon Chenery
+ * Copyright (C) 2009-2016 DANS - Data Archiving and  Networked Services (info@dans.knaw.nl)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
